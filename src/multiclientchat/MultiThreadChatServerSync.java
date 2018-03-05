f3f11e89d55590f156b3c24d62ea1f4f4b808653
ABHI\Abhijit <abhijitsojitra@gmail.com>
2018-03-06 00:30:44 +0530

    second commite

--- src/multiclientchat/MultiThreadChatServerSync.java
+++ src/multiclientchat/MultiThreadChatServerSync.java
@@ -1,5 +1,3 @@
-
-
 package multiclientchat;
 
 import java.io.PrintStream;
@@ -7,74 +5,80 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author mohammed
  */
-
 // the Server class
 public class MultiThreadChatServerSync {
-   // The server socket.
-  private static ServerSocket serverSocket = null;
-  // The client socket.
-  private static Socket clientSocket = null;
+    // The server socket.
 
-  // This chat server can accept up to maxClientsCount clients' connections.
-  private static final int maxClientsCount = 10;
-  private static final clientThread[] threads = new clientThread[maxClientsCount];
+    private static ServerSocket serverSocket = null;
+    // The client socket.
+    private static Socket clientSocket = null;
 
-  public static void main(String args[]) {
+    // This chat server can accept up to maxClientsCount clients' connections.
+    private static final int maxClientsCount = 10;
+    private static final clientThread[] threads = new clientThread[maxClientsCount];
 
-    // The default port number.
+    public static void main(String args[]) {
+
+        // The default port number.
 //    int portNumber = 3333;
-    int portNumber = 2222;    
-    if(args!=null && !args[0].equals("")){
-        portNumber = Integer.parseInt(args[0]);
-    }else{
-        JOptionPane.showMessageDialog(null, "Plsease enter port number");
-        System.exit(1);
-    }
-    
-    if (args.length < 1) {
-      System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
-          + "Now using port number=" + portNumber);
-    } else {
-      portNumber = Integer.valueOf(args[0]).intValue();
-    }
+        int portNumber = 2222;
+        if (args != null && !args[0].equals("")) {
+            portNumber = Integer.parseInt(args[0]);
+        } else {
+            JOptionPane.showMessageDialog(null, "Plsease enter port number");
+            System.exit(1);
+        }
 
-    /*
+        if (args.length < 1) {
+            System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
+                    + "Now using port number=" + portNumber);
+        } else {
+            portNumber = Integer.valueOf(args[0]).intValue();
+        }
+
+        /*
      * Open a server socket on the portNumber (default 2222). Note that we can
      * not choose a port less than 1023 if we are not privileged users (root).
-     */
-    try {
-      serverSocket = new ServerSocket(portNumber);
-    } catch (IOException e) {
-      System.out.println(e);
-    }
+         */
+        try {
+            serverSocket = new ServerSocket(portNumber);
 
-    /*
-     * Create a client socket for each connection and pass it to a new client
-     * thread.
-     */
-    while (true) {
-      try {
-        clientSocket = serverSocket.accept();
-        int i = 0;
-        for (i = 0; i < maxClientsCount; i++) {
-          if (threads[i] == null) {
-            (threads[i] = new clientThread(clientSocket, threads)).start();
-            break;
-          }
+                System.out.println("GET HTTP // 1.1 200 OK\r\n");
+                System.out.println("Connection Host : localhost:" + portNumber);
+                System.out.println("Content-Type: text/html");
+        } catch (IOException e) {
+            System.out.println(e);
         }
-        if (i == maxClientsCount) {
-          PrintStream os = new PrintStream(clientSocket.getOutputStream());
-          os.println("Server too busy. Try later.");
-          os.close();
-          clientSocket.close();
+
+        /*
+         * Create a client socket for each connection and pass it to a new client
+         * thread.
+         */
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+
+                int i = 0;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        (threads[i] = new clientThread(clientSocket, threads)).start();
+                        break;
+                    }
+                }
+                if (i == maxClientsCount) {
+                    PrintStream os = new PrintStream(clientSocket.getOutputStream());
+                    os.println("Server too busy. Try later.");
+                    os.close();
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
         }
-      } catch (IOException e) {
-        System.out.println(e);
-      }
     }
-  }  
 }
